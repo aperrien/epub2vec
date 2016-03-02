fa4549a44026b7b37fca7366947a4285d245d7aa
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-03-02 00:02:51 -0500

    Gather text from all xhtml files
    For each file store: bookid + filename + raw text

--- epubs2vec.py
+++ epubs2vec.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 import re
+from bs4 import BeautifulSoup
 files = os.listdir('.')
 
 epubs = [x for x in files if '.epub' in x]
@@ -10,3 +11,19 @@ for epub in epubs:
     print 'extracting ' + bookid + '...'
     with zipfile.ZipFile(epub,'r') as z:
         z.extractall('./temp/' + str(bookid))
+
+bookids = []
+filenames = []
+chapters = []
+
+print 'gathering xhtml files...'
+for root, dirs, files in os.walk('.'):
+    for filename in files:
+        if '.xhtml' in filename:
+            full_filepath = root + '/' + filename
+            bookids.append(re.sub(r'./(.*?)/.*',r'\1',root))
+            filenames.append(filename)
+            print 'gathering text from ' + re.sub(r'./(.*?)/.*',r'\1',root) + '\t' + filename
+            soup = BeautifulSoup(open(full_filepath), 'lxml')
+            filetext = soup.find('body').get_text()
+            chapters.append(filetext)
